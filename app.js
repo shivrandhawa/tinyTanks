cc8b2b6268ec07a6a52588b0828f23057cef26ce
Shiv <shivbirr@gmail.com>
2018-11-05 19:40:15 -0800

    working on it

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
-var express = require ('express');
-
+var express = require('express');
 var app = express();
-
 var server = app.listen(2000);
-
-
 app.use(express.static('client'))
 console.log("My socket server is running");
 
-var socket = require('socket.io');
 
-var io = socket(server);
+// io object now has all functionalities of socket io library //
+var io = require('socket.io')(server, {});
 
-io.sockets.on('connection', newConnection);
 
-function newConnection(socket){
+
+
+//////////////////////////////
+// ON NEW CONNECTION //
+//////////////////////////////
+io.sockets.on('connection', (socket) => {
+
+
     console.log('new connection: ' + socket.id);
-}
+
+    socket.on('samplemessage', (data) => {
+        console.log("sample emit recieved with msg: " + data.x);
+    });
+
+    socket.emit('msgfromserver',{
+        x: 'sample var',
+        y: 'sample var y',
+    });
+
+
+});
+
