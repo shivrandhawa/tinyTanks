444171c34371d47f3261b9ba51a859cdd5ba7116
Shiv <shivbirr@gmail.com>
2018-11-05 23:57:08 -0800

    update

--- app.js
+++ app.js
@@ -7,42 +7,32 @@ console.log("My socket server is running");
 
 var SOCKETS = {}; //list of all the sockets connect
 var PLAYERS = {}; //list of all players
+
 // io object now has all functionalities of socket io library //
+
+
+
 var io = require('socket.io')(server, {});
 
 //////////////////////////////
 // ON NEW CONNECTION //
+//////////////////////////////
 /* assign each socket a random id, then add socket to SOCKETS list */
 io.sockets.on('connection', (socket) => {
     socket.id = Math.random();//random id for each socket
     socket.SAMPLEVARIABLE = "SAMPLE";
     SOCKETS[socket.id] = socket;
-    //create player with the socketid:
-    var player = Player(socket.id);
-    PLAYERS[socket.id] = player;
-
+    Player.connect(socket);
     ////////////////////////
     // ON DISCONNECT EVENT//
     ////////////////////////
     socket.on('disconnect', () => {
         delete SOCKETS[socket.id];
-        delete PLAYERS[socket.id];
+        // Player.disconnect(socket); //not needed
+        
+        delete Player.list[socket.id];
 
     });
-
-    socket.on('keyPress', data => {
-        if (data.inputId === 'ups') {
-            player.UP = data.state;
-
-        }
-        else if (data.inputId === 'lefts')
-            player.LEFT = data.state;
-        else if (data.inputId === 'downs')
-            player.DOWN = data.state;
-        else if (data.inputId === 'rights')
-            player.RIGHT = data.state;
-
-    })
 
 });
 
@@ -99,26 +89,52 @@ var Player = function (id) {
         if (self.DOWN)
             self.y += self.maxSpd;
     }
+    Player.list[id] = self;
     return self;
 
 }
+Player.list = {}; //new method of holding the player list
 
+Player.connect = (socket) => {
+    var player = Player(socket.id);
 
+    socket.on('keyPress', data => {
+        if (data.inputId === 'ups')
+            player.UP = data.state;
 
+        else if (data.inputId === 'lefts')
+            player.LEFT = data.state;
+        else if (data.inputId === 'downs')
+            player.DOWN = data.state;
+        else if (data.inputId === 'rights')
+            player.RIGHT = data.state;
 
+    });
+};
+// Player.disconnect = socket =>{
+//     delete Player.list[socket.id]; //not needed unless other is needed
+// };
 
-setInterval(() => {
+Player.move = () => {
     //box contains information (position) about everyplayer in the game and sends it to every other player.
     var box = [];
-    for (var i in PLAYERS) {
-        var player = PLAYERS[i];        //cnt do let socket = cuz used outside scope.
-        player.moveUnit();
+    for (var i in Player.list) {
+        var player = Player.list[i];        //cnt do let socket = cuz used outside scope.
+        player.move();
         box.push({
             x: player.x,
             y: player.y,
             number: player.number
         });
     }
+    return box;
+}
+
+setInterval(() => {
+
+   var box = Player.move();
+
+    
     for (var i in SOCKETS) {
         var socket = SOCKETS[i]
         socket.emit('updatePos', box);  //emit box to all client
