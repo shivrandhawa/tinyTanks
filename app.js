5ca10a47a2b6f4084d89d412f272163c142739f2
Shiv <shivbirr@gmail.com>
2018-11-05 23:26:28 -0800

    updating

--- app.js
+++ app.js
@@ -1,59 +1,130 @@
+
 var express = require('express');
 var app = express();
 var server = app.listen(2000);
 app.use(express.static('client'))
 console.log("My socket server is running");
 
-//list of all the sockets connect
-var SOCKETS = {}
+var SOCKETS = {}; //list of all the sockets connect
+var PLAYERS = {}; //list of all players
 // io object now has all functionalities of socket io library //
 var io = require('socket.io')(server, {});
 
-
-
-
 //////////////////////////////
 // ON NEW CONNECTION //
-//////////////////////////////
+/* assign each socket a random id, then add socket to SOCKETS list */
 io.sockets.on('connection', (socket) => {
-
-    console.log('new connection: ' + socket.id);
-    socket.id = Math.floor(Math.random() * 10);//random id for each socket
-    console.log('same connec ' + socket.id);
-    socket.x = 0;              //socket x coord
-    socket.y = 0;              //socket y coord;
-    socket.SAMPLEVARIABLE = "SAMPLE"
+    socket.id = Math.random();//random id for each socket
+    socket.SAMPLEVARIABLE = "SAMPLE";
     SOCKETS[socket.id] = socket;
-    ////////////////////////////////////
-    // PRACTICE EMIT AND RECIEVE CODE //
-    ////////////////////////////////////
-    socket.on('samplemessage', (data) => {
-        console.log("sample emit recieved with msg: " + data.x);
+    //create player with the socketid:
+    var player = Player(socket.id);
+    PLAYERS[socket.id] = player;
+
+    ////////////////////////
+    // ON DISCONNECT EVENT//
+    ////////////////////////
+    socket.on('disconnect', () => {
+        delete SOCKETS[socket.id];
+        delete PLAYERS[socket.id];
+
     });
 
-    //this will be displayed in the chrome console.
-    socket.emit('msgfromserver',{
-        x: 'sample var',
-        y: 'sample var y',
-    });
+    socket.on('keyPress', data => {
+        if (data.inputId === 'ups') {
+            player.UP = data.state;
 
+        }
+        else if (data.inputId === 'lefts')
+            player.LEFT = data.state;
+        else if (data.inputId === 'downs')
+            player.DOWN = data.state;
+        else if (data.inputId === 'rights')
+            player.RIGHT = data.state;
+
+    })
 
 });
+
+//////////////////
+// THING OBJECT //
+//////////////////
+var Thing = function () {
+    var self = {
+        x: 250,  //center of play area
+        y: 250,
+        id: " ",
+        xspeed: 0,
+        yspeed: 0,
+    }
+    self.move = () => {
+        self.moveUnit();
+    }
+    self.moveUnit = () => {
+        self.x += self.xspeed
+        self.y += self.yspeed
+
+    }
+    return self;
+}
+
+
+//////////////////
+// PLAYER OBJECT //
+//////////////////
+var Player = function (id) {
+    var self = Thing();
+    self.id = id;
+    self.number = "" + Math.floor(10 * Math.random());
+    self.LEFT = false;
+    self.RIGHT = false;
+    self.DOWN = false;
+    self.UP = false;
+    self.maxSpd = 10; //TODO: change var
+
+
+    var su_update = self.move;
+    self.move = () => {
+        self.moveUnit();
+        su_update(); //TODO: change var
+    }
+
+    self.moveUnit = () => {
+        if (self.RIGHT)
+            self.x += self.maxSpd;
+        if (self.LEFT)
+            self.x -= self.maxSpd;
+        if (self.UP)
+            self.y -= self.maxSpd;
+        if (self.DOWN)
+            self.y += self.maxSpd;
+    }
+    return self;
+
+}
+
+
 
 
 
 setInterval(() => {
-   /*  for each socket update x and y, emit update 
-   position with new x and y */
-    for (var i in SOCKETS){
-        let socket = SOCKETS[i];
-        socket.x++;
-        socket.y++;
-        socket.emit('updatePos',{
-            x:socket.x,
-            y:socket.y,
+    //box contains information (position) about everyplayer in the game and sends it to every other player.
+    var box = [];
+    for (var i in PLAYERS) {
+        var player = PLAYERS[i];        //cnt do let socket = cuz used outside scope.
+        player.moveUnit();
+        box.push({
+            x: player.x,
+            y: player.y,
+            number: player.number
         });
     }
+    for (var i in SOCKETS) {
+        var socket = SOCKETS[i]
+        socket.emit('updatePos', box);  //emit box to all client
 
-}),1000/25;
+    }
+
+
+}, 1000 / 25);
 
